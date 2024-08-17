465bc447529d716ccaae8094b8d6c5b34c08fb94
Omor Faruk Mahfuj <mypcfaruk+github@gmail.com>
2024-08-17 23:07:39 +0600

    custom font family added

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -1,7 +1,12 @@
+import Header from "./shared/Header";
+import Navbar from './shared/Navbar';
+
 const Home = () => {
   return (
     <div>
-      <h2>This is Home</h2>
+      <Header></Header>
+      <Navbar></Navbar>
+      <h2 className="text-3xl font-bold">This is Home</h2>
     </div>
   );
 };
---
+++ src/assets/pages/shared/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h2 className="text-3xl">Header</h2>
+    </header>
+  );
+};
+
+export default Header;
