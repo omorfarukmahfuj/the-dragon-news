fb93d70794b26052d8c1168d1fe8800db43f7a6a
Omor Faruk Mahfuj <mypcfaruk+github@gmail.com>
2024-08-19 01:17:04 +0600

    sidebar categories added

--- src/pages/shared/LeftSideNav.jsx
+++ src/pages/shared/LeftSideNav.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
 
 const LeftSideNav = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch('categories.json')
+      .then(res => res.json())
+      .then(data => setCategories(data))
+  }, [])
   return (
     <div>
-      <h2 className="text-2xl">Left Side</h2>
+      {/* All Categories */}
+      <div className='mb-7'>
+        <h2 className="text-xl text-[#403F3F] font-semibold mb-5">All Categories</h2>
+        {
+          categories.map(category => <NavLink className='block font-medium text-xl text-[#9F9F9F] mb-7' key={category.id} to={`/category/${category.id}`}>{category.name}</NavLink>)
+        }
+
+      </div>
     </div>
   );
 };
